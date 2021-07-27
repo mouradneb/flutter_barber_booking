ef986c6c0a7b9f72d084116fb620dbe1011ed1c6
mouradneb <mourad.nebhi@gmail.com>
2021-07-27 22:05:12 +0100

    Firebase authentication added

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,25 @@
+// @dart=2.9
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_ui/firebase_auth_ui.dart';
+import 'package:firebase_auth_ui/providers.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barber_booking/state/state_management.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -28,27 +28,19 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  GlobalKey<ScaffoldState> scaffoldState = new GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldState,
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
@@ -63,7 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
               width: MediaQuery.of(context).size.width,
               padding: EdgeInsets.all(16),
               child: ElevatedButton.icon(
-                onPressed: () {},
+                onPressed: () => processLogin(context),
                 icon: Icon(Icons.phone, color: Colors.white),
                 label: Text('LOGIN WITH PHONE',
                     style: TextStyle(color: Colors.white)),
@@ -76,5 +68,33 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+
+  processLogin(BuildContext context) {
+    var user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) {
+      //user not login, show login
+      FirebaseAuthUi.instance()
+          .launchAuth([AuthProvider.phone()]).then((firebaseUser) {
+        //refresh state
+        context.read(userLogged).state = FirebaseAuth.instance.currentUser;
+        ScaffoldMessenger.of(scaffoldState.currentContext ?? context)
+            .showSnackBar(SnackBar(
+                content: Text(
+                    'Login success ${FirebaseAuth.instance.currentUser?.phoneNumber}')));
+      }).catchError((e) {
+        if (e is PlatformException) if (e.code ==
+            FirebaseAuthUi.kUserCancelledError)
+          ScaffoldMessenger.of(scaffoldState.currentContext ?? context)
+              .showSnackBar(SnackBar(content: Text('${e.message}')));
+        else
+          ScaffoldMessenger.of(scaffoldState.currentContext ?? context)
+              .showSnackBar(SnackBar(content: Text('Unknown Error')));
+      });
+    } else {
+      //user already login
+
+    }
   }
 }
